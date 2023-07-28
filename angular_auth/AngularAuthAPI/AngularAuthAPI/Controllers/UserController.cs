3b656fd84fd258c949f7e4ea3014d3f8be3bcf81
Alvaro <93229518+Alvaro2003j@users.noreply.github.com>
2023-07-27 21:19:08 -0500

    implement-project
    insert the project

---
+++ angular_auth/AngularAuthAPI/AngularAuthAPI/Controllers/UserController.cs
@@ -0,0 +1,95 @@
+﻿using AngularAuthAPI.Context;
+using AngularAuthAPI.Helpers;
+using AngularAuthAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AngularAuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+
+        public UserController(AppDbContext appDbContext)
+        {
+            _authContext = appDbContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+            var user = await _authContext.Users
+                .FirstOrDefaultAsync(x => x.Username == userObj.Username);
+            if (user == null)
+                return NotFound(new { Message = "User Not Found!" });
+
+            if (!PasswordHasher.verifyPassword(userObj.Password, user.Password))
+                return BadRequest(new { Message = "Password is Incorrect"});
+
+            return Ok(new
+            {
+                Message = "Login Success!"
+            });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            //Check username similar
+            if (await CheckUserNameExistAsync(userObj.Username))
+                return BadRequest(new { Message = "UserName Already Exists!! You need to use other username"});
+
+            //Check email similar
+            if (await CheckEmailExistAsync(userObj.Email))
+                return BadRequest(new { Message = "Email Already Exists!! You need to use other email" });
+
+            //Check password Strength
+            var pass = CheckPasswordStrength(userObj.Password);
+            if (!string.IsNullOrEmpty(pass))
+                return BadRequest(new { Message = pass });
+
+            userObj.Password = PasswordHasher.hashPassword(userObj.Password);
+            userObj.Role = "User";
+            userObj.Token = "";
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new {
+                Message = "User Registered!"
+            });
+        }
+
+
+        private Task<bool> CheckUserNameExistAsync(string username) => _authContext.Users.AnyAsync(x => x.Username == username);
+
+        private Task<bool> CheckEmailExistAsync(string email) => _authContext.Users.AnyAsync(x => x.Email == email);
+        
+        private string CheckPasswordStrength(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (password.Length < 8) //Contraseña debe tener más de 8 caracteres
+                sb.Append("Minimun password length should be 8 characteres" + Environment.NewLine);
+
+            if (!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]") && Regex.IsMatch(password, "[0-9]"))) //Contraseña de tipo Alfanúmerico
+                sb.Append("Password should be Aphanumeric" + Environment.NewLine);
+
+            if (!Regex.IsMatch(password, "[<,>,@,!,¡,#,$,%,^,&,*,°,¬,(,),_,+,\\[,\\],{,},?,¿,:,;,|,',\\,.,/,~,`,-,=,\\,]")) //La contraseña no debe incluir caracteres especiales
+                sb.Append("Password should contain special chars" + Environment.NewLine);
+
+            return sb.ToString();
+        }
+    }
+}
